828fbc269f22b3a03d4b8335602c7cbb214bf19e
Mohit Kunwar <mk3581@srmist.edu.in>
2026-02-10 20:48:59 +0530

    Refactor Pong game to a Turtle crossing game

--- main.py
+++ main.py
@@ -1,55 +1,38 @@
-from turtle import Screen
-from paddle import Paddle
-from ball import Ball
-from scoreboard import Scoreboard
 import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
-# Screen setup
 screen = Screen()
-screen.setup(width=800, height=600)
-screen.bgcolor("black")
-screen.title("Pong Game")
+screen.setup(width=600, height=600)
 screen.tracer(0)
 
-# Create objects
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
-ball = Ball()
+player = Player()
+car_manager = CarManager()
 scoreboard = Scoreboard()
 
-# Keyboard bindings
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+screen.onkey(player.move, "Up")
 
-# Game loop
 game_is_on = True
 while game_is_on:
-    time.sleep(ball.move_speed)
+    time.sleep(0.1)
     screen.update()
-    ball.move()
 
-    # Bounce off top & bottom walls
-    if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce_y()
+    car_manager.create_car()
+    car_manager.move_cars()
 
-    # Paddle collision
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
-        ball.bounce_x()
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
 
-    if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
-        ball.bounce_x()
-
-    # Right paddle misses
-    if ball.xcor() > 380:
-        ball.reset_position()
-        scoreboard.l_point()
-
-    # Left paddle misses
-    if ball.xcor() < -380:
-        ball.reset_position()
-        scoreboard.r_point()
+    # Detect successful crossing
+    if player.at_finish_line():
+        player.reset_position()
+        car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
